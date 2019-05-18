1ee5538c1796976108df24af3172a9cc52a7e3c9
driwazz <49757775+driwazz@users.noreply.github.com>
2019-05-18 02:36:22 +0000

    final version authority model-entity

--- src/main/java/com/unitedremote/springboot/entities/Authorities.java
+++ src/main/java/com/unitedremote/springboot/entities/Authorities.java
@@ -1,5 +1,7 @@
 package com.unitedremote.springboot.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,25 +11,23 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import com.unitedremote.springboot.entities.User;
 
 @Entity
-@Table(name = "authorities", catalog = "unitedremote", 
-        uniqueConstraints = @UniqueConstraint(columnNames = { "authority","username" }))
-public class Authorities {
+@Table(name = "authorities")
+public class Authorities implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	@Column(name = "user_role_id", unique = true, nullable = false)
 	private Long userRoleId;
 	
-	@Column(name = "authority", nullable = false, length = 45)
+	@Column(name = "authority", nullable = false, length = 50)
 	private String authority;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "username", nullable = false)
+	@JoinColumn(name = "username", nullable = false, unique = true)
 	private User user;
 	
 	
