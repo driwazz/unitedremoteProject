4678c3a3bfef13c2fb7943043c00424cec56cb6f
driwazz <49757775+driwazz@users.noreply.github.com>
2019-05-18 02:34:02 +0000

    beta-final version webcontroller

--- src/main/java/com/unitedremote/springboot/controller/WebController.java
+++ src/main/java/com/unitedremote/springboot/controller/WebController.java
@@ -1,21 +1,44 @@
 package com.unitedremote.springboot.controller;
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.unitedremote.springboot.entities.User;
-import com.unitedremote.springboot.service.AuthorityService;
-import com.unitedremote.springboot.service.UserService;
+import com.unitedremote.springboot.service.Webservice;
 
 @Controller
 public class WebController {
 	
+	@Autowired
+	Webservice webservice;
+   
+	public String currentUser;
+	
 	@RequestMapping(value="/")
-    public String home(){
+    public String home(Model model){
+		currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
+		model.addAttribute("shopList", webservice.selectShops(currentUser,false));
+        return "home";
+    }
+	
+	@RequestMapping(value="/{id}")
+    public String likedShop(@PathVariable Long id, Model model){
+		webservice.likedShopsServ(id);
+		model.addAttribute("shopList", webservice.selectShops(currentUser,false));
         return "home";
     }
 	
@@ -24,16 +47,11 @@ public class WebController {
 		return "signup";
 	}
 	
-	@GetMapping("/signed")
-	public String signedG() {
+	@PostMapping("/signed")
+	public String signedG(@ModelAttribute("usser") User user) {
+		webservice.register(user);
 		return "signed";
 	}
-   
-    @RequestMapping(value="/user")
-    public String user(){
-        return "user";
-    }
-  
    
     @RequestMapping(value="/login")
     public String login(){
@@ -46,10 +64,17 @@ public class WebController {
     }
     
     @GetMapping("/likedShops")
-	public String liked() {
-		
+	public String liked(Model model) {
+    	model.addAttribute("shopListliked", webservice.selectShops(currentUser,true));
 		return "myprefrred";
 	}
+    
+    @RequestMapping(value="/likedShops/{id}")
+    public String dislikedShop(@PathVariable Long id, Model model){
+		webservice.dislikedShopsServ(id);
+		model.addAttribute("shopListliked", webservice.selectShops(currentUser,true));
+        return "redirect:/likedShops";
+    }
     
     @RequestMapping(value="/logout", method = RequestMethod.GET)
     public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
@@ -59,5 +84,4 @@ public class WebController {
         }
         return "redirect:/login";
     }
-
 }
