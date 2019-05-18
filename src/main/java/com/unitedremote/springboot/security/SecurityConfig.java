1c3598c9438adbe81cf63ef448a93fefce90ee67
driwazz <49757775+driwazz@users.noreply.github.com>
2019-05-18 02:42:53 +0000

    Security Part (authentification/authorization)  beta-version

--- src/main/java/com/unitedremote/springboot/security/SecurityConfig.java
+++ src/main/java/com/unitedremote/springboot/security/SecurityConfig.java
@@ -6,42 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	DataSource dataSource;
 
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
-        .usersByUsernameQuery("select username, password, enabled from users where username=?");
-        
-		}
-
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+				/*.passwordEncoder(passwordEncoder())*/;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//close access & display my custom login page 
 		http.authorizeRequests()
-		      .anyRequest().authenticated().antMatchers("/home","/likedShops").hasRole("USER")
-		      .anyRequest().authenticated()
-		      .and().formLogin().loginPage("/login").permitAll()
-		      .and().logout()
-		      .permitAll();
-		      
+	      .anyRequest().authenticated().antMatchers("/home","/likedShops").hasRole("USER")
+	      .anyRequest().authenticated()
+	      .and().formLogin().loginPage("/login").permitAll()
+	      .and()
+	      .logout()
+          .logoutSuccessUrl("/login");
+		http.exceptionHandling().accessDeniedPage("/403");
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		//enable these pages to be accessible even using spring security
@@ -49,10 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	    web.ignoring().antMatchers("/signup");
 	}
 	
-	@Bean
-	BCryptPasswordEncoder passwordEncoder() {
-	return new BCryptPasswordEncoder();
-	}
-	
-
+	/*@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}*/
 }
